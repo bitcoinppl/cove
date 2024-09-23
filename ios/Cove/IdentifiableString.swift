9283e85eedc847460cb89fca7311c7dfe2a39943
Praveen Perera <me@praveenperera.com>
2024-09-23 14:48:46 -0500

    Create IdentifiableString.swift

---
+++ ios/Cove/IdentifiableString.swift
@@ -0,0 +1,14 @@
+//
+//  IdentifiableString.swift
+//  Cove
+//
+//  Created by Praveen Perera on 9/23/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct IdentifiableString: Identifiable, Equatable {
+    let id = UUID()
+    let value: String
+}
