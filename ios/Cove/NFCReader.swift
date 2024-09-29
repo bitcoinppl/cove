312f773b5d7d73904cc05a3bde246b1ad98a8024
Praveen Perera <me@praveenperera.com>
2024-09-29 16:53:50 -0500

    Function to read block by block

--- ios/Cove/NFCReader.swift
+++ ios/Cove/NFCReader.swift
@@ -14,6 +14,7 @@ import CoreNFC
 class NFCReader: NSObject, NFCTagReaderSessionDelegate {
     var session: NFCTagReaderSession?
     var scannedMessage: String?
+    var retries = 0
     
     func scan() {
         session = NFCTagReaderSession(pollingOption: [.iso14443, .iso15693], delegate: self)
@@ -22,6 +23,7 @@ class NFCReader: NSObject, NFCTagReaderSessionDelegate {
     }
     
     func tagReaderSession(_ session: NFCTagReaderSession, didDetect tags: [NFCTag]) {
+        print("tags count: \(tags.count)")
         guard let tag = tags.first else {
             session.invalidate(errorMessage: "No tag detected.")
             return
@@ -36,7 +38,7 @@ class NFCReader: NSObject, NFCTagReaderSessionDelegate {
             switch tag {
             case .iso15693(let iso15693Tag):
                 Log.debug("found tag iso15693: \(iso15693Tag)")
-                self.readNDEF(from: iso15693Tag, session: session)
+                self.readIso15693Tag(from: iso15693Tag, session: session)
             case .iso7816(let iso7816Tag):
                 Log.debug("found tag iso7816")
                 self.readNDEF(from: iso7816Tag, session: session)
@@ -53,6 +55,79 @@ class NFCReader: NSObject, NFCTagReaderSessionDelegate {
         }
     }
     
+    func readIso15693Tag(from tag: NFCISO15693Tag, session: NFCTagReaderSession) {
+        print("reading iso tag large....")
+        session.alertMessage = "Reading data please hold still..."
+        readNDEF(from: tag, session: session)
+//        tag.getSystemInfo(requestFlags: .highDataRate) { result in
+//            switch result {
+//            case .success(let systemInfo):
+//                print("systemInfo: \(systemInfo)")
+//                print("Block Size: \(systemInfo.blockSize)")
+//                print("Total Blocks: \(systemInfo.totalBlocks)")
+//
+//            case .failure(let error):
+//                print("Error getting system info: \(error.localizedDescription)")
+//            }
+//        }
+//
+//        readBlocks(tag: tag, session: session)
+    }
+    
+    func readBlocks(tag: NFCISO15693Tag, session: NFCTagReaderSession) {
+        print("read blocks....")
+        var allData = Data()
+        var currentBlock = 0
+        
+        func readNextBlock() {
+            if currentBlock >= 255 {
+                currentBlock = 0
+            }
+            
+            print("current block: \(currentBlock)")
+            
+            tag.readSingleBlock(requestFlags: .highDataRate, blockNumber: UInt8(currentBlock)) { result in
+                switch result {
+                case .success(let data):
+                    print("read data block: \(data.base64EncodedString())")
+                    let bytes = [UInt8](data)
+                    print("bytes: \(bytes)")
+                    allData.append(data)
+                    currentBlock += 1
+                    readNextBlock()
+                case .failure(let error):
+                    print("Error reading block \(currentBlock): \(error.localizedDescription)")
+                    session.invalidate(errorMessage: "Error reading tag")
+                }
+            }
+        }
+        
+        readNextBlock()
+    }
+
+//
+//    func readAllBlocks(tag: NFCISO15693Tag, blockSize: Int, blockCount: Int, session: NFCTagReaderSession) {
+//        var data = Data()
+//        let group = DispatchGroup()
+//
+//        for i in 0 ..< blockCount {
+//            group.enter()
+//            tag.readSingleBlock(requestFlags: .highDataRate, blockNumber: UInt8(i)) { blockData, error in
+//                defer { group.leave() }
+//                guard let blockData = blockData, error == nil else {
+//                    return
+//                }
+//                data.append(blockData)
+//            }
+//        }
+//
+//        group.notify(queue: .main) {
+    ////            self.processTagData(data)
+//            session.alertMessage = "Tag read successfully!"
+//            session.invalidate()
+//        }
+//    }
+
     func readNDEF<T: NFCNDEFTag>(from tag: T, session: NFCTagReaderSession) {
         Log.debug("reading NDEF message from tag: \(tag)")
         
@@ -61,7 +136,6 @@ class NFCReader: NSObject, NFCTagReaderSessionDelegate {
                 if message == nil {
                     Log.error("read error: \(error.localizedDescription)")
                     session.invalidate(errorMessage: "Unable to read NFC tag please try again.")
-                    session.restartPolling()
                     return
                 }
             }
@@ -69,7 +143,6 @@ class NFCReader: NSObject, NFCTagReaderSessionDelegate {
             guard let message = message else {
                 Log.error("no NDEF message found")
                 session.invalidate(errorMessage: "Unable to read NFC tag please try again.")
-                session.restartPolling()
                 return
             }
             
