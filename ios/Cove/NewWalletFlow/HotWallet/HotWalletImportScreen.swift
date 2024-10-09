fe49f9adb4d9bd28fec5de79580bf2c1e811dcd6
Praveen Perera <me@praveenperera.com>
2024-10-08 20:01:59 -0500

    Add delay before scanning wallet to prevent extra scans

--- ios/Cove/NewWalletFlow/HotWallet/HotWalletImportScreen.swift
+++ ios/Cove/NewWalletFlow/HotWallet/HotWalletImportScreen.swift
@@ -45,7 +45,6 @@ struct HotWalletImportScreen: View {
     @State private var tasks: [Task<Void, any Error>] = []
 
     func initOnAppear() {
-        Log.info("initOnAppear: \(importType), routes: \(app.router.routes) || \(app.router.default)")
         nfcReader = NFCReader()
 
         switch importType {
@@ -112,11 +111,11 @@ struct HotWalletImportScreen: View {
         do {
             let multiQr: MultiQr =
                 try self.multiQr
-                    ?? {
-                        let newMultiQr = try MultiQr.tryNew(qr: qr)
-                        self.multiQr = newMultiQr
-                        return newMultiQr
-                    }()
+                ?? {
+                    let newMultiQr = try MultiQr.tryNew(qr: qr)
+                    self.multiQr = newMultiQr
+                    return newMultiQr
+                }()
 
             // see if its single qr or seed qr
             if let words = try multiQr.getGroupedWords(qr: qr, groupsOf: UInt8(6)) {
@@ -179,7 +178,8 @@ struct HotWalletImportScreen: View {
                         outerIndex = outerIndex - 1
                     }
 
-                    if innerIndex > 5 || outerIndex > lastIndex || outerIndex < 0 || innerIndex < 0 {
+                    if innerIndex > 5 || outerIndex > lastIndex || outerIndex < 0 || innerIndex < 0
+                    {
                         Log.error(
                             "Something went wrong: innerIndex: \(innerIndex), outerIndex: \(outerIndex), lastIndex: \(lastIndex), focusField: \(focusField)"
                         )
@@ -369,7 +369,7 @@ struct HotWalletImportScreen: View {
             // if its the last word on the non last card and all words are valid words, then go to next tab
             // focusField will already have changed by now
             if let focusField = self.focusField,
-               !buttonIsDisabled && tabIndex < lastIndex && focusField % 6 == 1
+                !buttonIsDisabled && tabIndex < lastIndex && focusField % 6 == 1
             {
                 withAnimation {
                     tabIndex += 1
@@ -609,7 +609,7 @@ private struct AutocompleteField: View {
             // then auto select the first selection, because we want auto selection
             // but also allow the user to fix a wrong word
             if let word = filteredSuggestions.last,
-               filteredSuggestions.count == 1 && oldText.count < newText.count
+                filteredSuggestions.count == 1 && oldText.count < newText.count
             {
                 state = .valid
                 filteredSuggestions = []
