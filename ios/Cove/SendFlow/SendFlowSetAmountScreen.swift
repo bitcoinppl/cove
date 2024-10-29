6959022b503c33b440a3055de126128ad134c8fa
Praveen Perera <me@praveenperera.com>
2024-10-29 12:15:01 -0500

    Create new color, image and SendFlow

---
+++ ios/Cove/SendFlow/SendFlowSetAmountScreen.swift
@@ -0,0 +1,19 @@
+//
+//  SendFlowSetAmountScreen.swift
+//  Cove
+//
+//  Created by Praveen Perera on 10/29/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct SendFlowSetAmountScreen: View {
+    var body: some View {
+        Text("SendFlowSetAmountScreen")
+    }
+}
+
+#Preview {
+    SendFlowSetAmountScreen()
+}
