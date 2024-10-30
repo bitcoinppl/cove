b2e6ac5caef4414af70e4b13de4e3f38d41f1787
Praveen Perera <me@praveenperera.com>
2024-10-29 21:44:30 -0500

    Create header for sending flow

---
+++ ios/Cove/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Cove
+//
+//  Created by Praveen Perera on 10/29/24.
+//
+
+import SwiftUI
+
+let screenHeight = UIScreen.main.bounds.height
+let screenWidth = UIScreen.main.bounds.width
--- ios/Cove/SendFlow/SendFlowSetAmountScreen.swift
+++ ios/Cove/SendFlow/SendFlowSetAmountScreen.swift
@@ -9,11 +9,57 @@ import Foundation
 import SwiftUI
 
 struct SendFlowSetAmountScreen: View {
+    func setToolbarAppearence() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithTransparentBackground()
+        appearance.backgroundColor = UIColor.clear
+        appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().compactAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+        UINavigationBar.appearance().tintColor = .white
+    }
+
     var body: some View {
-        Text("SendFlowSetAmountScreen")
+        VStack(spacing: 0) {
+            ZStack {
+                VStack {
+                    HStack {
+                        Text("Balance")
+                    }
+                }
+                .background(
+                    Image(.headerPattern)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 400, height: 300,
+                               alignment: .topTrailing)
+                        .clipped()
+                        .ignoresSafeArea(.all)
+                )
+                .foregroundStyle(.white)
+                .ignoresSafeArea(.all)
+                .frame(width: screenWidth, height: screenHeight * 0.20)
+            }
+            .background(Color.midnightBlue)
+
+            VStack {
+                Text("SendFlowSetAmountScreen")
+                Spacer()
+            }
+
+            Spacer()
+        }
+        .navigationTitle("Send")
+        .navigationBarTitleDisplayMode(.inline)
+        .onAppear(perform: setToolbarAppearence)
     }
 }
 
 #Preview {
-    SendFlowSetAmountScreen()
+    NavigationStack {
+        SendFlowSetAmountScreen()
+    }
 }
