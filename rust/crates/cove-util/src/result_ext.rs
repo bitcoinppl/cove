73f1b9e72ff89dce29036f63282214372faa9c20
Praveen Perera <me@praveenperera.com>
2025-09-18 15:56:53 -0500

    Refactor ResultExt trait for improved flexibility

--- rust/crates/cove-util/src/result_ext.rs
+++ rust/crates/cove-util/src/result_ext.rs
@@ -1,10 +1,10 @@
 use std::fmt::Display;
 
-pub trait ResultExt<T, E> {
+pub trait ResultExt<T, InitialError> {
     /// Map an error to a string-based error variant
     ///
-    /// This allows converting `Result<T, E>` to `Result<T, F>` where F has a variant
-    /// that takes a String, using the Display implementation of E.
+    /// This allows converting `Result<T, InitialError>` to `Result<T, FinalError>` where FinalError has a variant
+    /// that takes a String, using the Display implementation of InitialError.
     ///
     /// # Example
     /// ```
@@ -22,29 +22,33 @@ pub trait ResultExt<T, E> {
     ///     Ok(())
     /// }
     /// ```
-    fn map_err_str<F>(self, f: fn(String) -> F) -> Result<T, F>
+    fn map_err_str<FinalError, F>(self, f: F) -> Result<T, FinalError>
     where
-        E: Display;
+        InitialError: Display,
+        F: FnOnce(String) -> FinalError;
 
     /// map an error using Into::into before passing to error constructor
     ///
     /// shorthand for map_err(|e| f(e.into())) to convert errors using Into trait
-    fn map_err_into<F, G>(self, f: fn(F) -> G) -> Result<T, G>
+    fn map_err_into<I, FinalError, F>(self, f: F) -> Result<T, FinalError>
     where
-        E: Into<F>;
+        InitialError: Into<I>,
+        F: FnOnce(I) -> FinalError;
 }
 
-impl<T, E> ResultExt<T, E> for Result<T, E> {
-    fn map_err_str<F>(self, f: fn(String) -> F) -> Result<T, F>
+impl<Type, InitialError> ResultExt<Type, InitialError> for Result<Type, InitialError> {
+    fn map_err_str<FinalError, F>(self, f: F) -> Result<Type, FinalError>
     where
-        E: Display,
+        InitialError: Display,
+        F: FnOnce(String) -> FinalError,
     {
         self.map_err(|e| f(e.to_string()))
     }
 
-    fn map_err_into<F, G>(self, f: fn(F) -> G) -> Result<T, G>
+    fn map_err_into<I, FinalError, F>(self, f: F) -> Result<Type, FinalError>
     where
-        E: Into<F>,
+        InitialError: Into<I>,
+        F: FnOnce(I) -> FinalError,
     {
         self.map_err(|e| f(e.into()))
     }
