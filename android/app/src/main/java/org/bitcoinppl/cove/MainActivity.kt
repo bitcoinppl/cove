850035ba32aea9de7b8522725c9915ecaece25e4
Praveen Perera <me@praveenperera.com>
2025-10-10 13:01:34 -0500

    Add ImportWalletManager and integrate wallet import flow

--- android/app/src/main/java/org/bitcoinppl/cove/MainActivity.kt
+++ android/app/src/main/java/org/bitcoinppl/cove/MainActivity.kt
@@ -1,17 +1,31 @@
 package org.bitcoinppl.cove
 
-import CoveApp
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import org.bitcoinppl.cove.import_wallet.ImportWalletScreen
 import org.bitcoinppl.cove.ui.theme.CoveTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
         setContent {
             CoveTheme {
-                CoveApp()
+                ImportWalletScreen(
+                    totalWords = 12,
+                    onBackClick = {
+                        Log.d("MainActivity", "Back clicked")
+                        // TODO: navigate back or finish activity
+                    },
+                    onImportSuccess = {
+                        Log.d("MainActivity", "Import success")
+                        // TODO: navigate to wallet screen
+                    }
+                )
             }
         }
     }
