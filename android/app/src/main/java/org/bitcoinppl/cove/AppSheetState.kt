034f867e9a3097e61998bda29dc6141928994e4b
Praveen Perera <me@praveenperera.com>
2025-10-20 11:08:32 -0500

    Add AppSheetState sealed class for sheet management

---
+++ android/app/src/main/java/org/bitcoinppl/cove/AppSheetState.kt
@@ -0,0 +1,13 @@
+package org.bitcoinppl.cove
+
+import org.bitcoinppl.cove_core.*
+
+/**
+ * represents different sheet states that can be presented in the app
+ * ported from iOS AppSheetState.swift
+ */
+sealed class AppSheetState {
+    data object Qr : AppSheetState()
+
+    data class TapSigner(val route: TapSignerRoute) : AppSheetState()
+}
