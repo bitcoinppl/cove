3bf669b3d4ed2e3e7cdd7b0f135630b36a03f6ba
Praveen Perera <me@praveenperera.com>
2025-10-29 11:15:14 -0500

    Optimize settings screens with remember for static lists

--- android/app/src/main/java/org/bitcoinppl/cove/settings/FiatCurrencySettingsScreen.kt
+++ android/app/src/main/java/org/bitcoinppl/cove/settings/FiatCurrencySettingsScreen.kt
@@ -26,6 +26,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -46,7 +47,7 @@ fun FiatCurrencySettingsScreen(
     app: org.bitcoinppl.cove.AppManager,
     modifier: Modifier = Modifier,
 ) {
-    val fiatCurrencies = allFiatCurrencies()
+    val fiatCurrencies = remember { allFiatCurrencies() }
     val selectedFiatCurrency = app.selectedFiatCurrency
 
     Scaffold(
