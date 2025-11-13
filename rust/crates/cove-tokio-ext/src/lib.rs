8d0d0eeda5467d7328494275bea9f2b8ff8e19ee
Praveen Perera <me@praveenperera.com>
2025-11-13 13:55:08 -0600

    Add cove-tokio-ext crate with Future timeout extensions

---
+++ rust/crates/cove-tokio-ext/src/lib.rs
@@ -0,0 +1,18 @@
+use std::future::Future;
+use std::time::Duration;
+use tokio::time;
+
+/// Blanket extension trait: implemented for *all* futures.
+pub trait FutureTimeoutExt: Future + Sized {
+    /// Wrap this future in a Tokio timeout.
+    fn with_timeout(self, dur: Duration) -> time::Timeout<Self> {
+        time::timeout(dur, self)
+    }
+
+    /// Wrap this future in a timeout that ends at an absolute deadline.
+    fn with_deadline(self, deadline: time::Instant) -> time::Timeout<Self> {
+        time::timeout_at(deadline, self)
+    }
+}
+
+impl<F: Future> FutureTimeoutExt for F {}
