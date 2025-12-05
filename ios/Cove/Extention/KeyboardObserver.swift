317e59b8f63669d529baf3a455d5b51a32562221
Praveen Perera <me@praveenperera.com>
2025-12-05 11:13:05 -0600

    Add custom keyboard toolbar for iOS

--- ios/Cove/Extention/KeyboardObserver.swift
+++ ios/Cove/Extention/KeyboardObserver.swift
@@ -2,28 +2,51 @@ import Foundation
 import SwiftUI
 import UIKit
 
+@Observable
 @MainActor
-class KeyboardObserver: ObservableObject {
-    @Published var keyboardIsShowing = false
+final class KeyboardObserver {
+    var keyboardIsShowing = false
+    var keyboardHeight: CGFloat = 0
+
+    // nonisolated so they can be accessed in deinit
+    private nonisolated(unsafe) var showObserver: NSObjectProtocol?
+    private nonisolated(unsafe) var hideObserver: NSObjectProtocol?
 
     init() {
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
-    }
+        showObserver = NotificationCenter.default.addObserver(
+            forName: UIResponder.keyboardWillShowNotification,
+            object: nil,
+            queue: .main
+        ) { [weak self] notification in
+            guard let self else { return }
+            let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect
 
-    @objc func keyboardWillShow() {
-        Task { @MainActor in
-            withAnimation(.easeInOut(duration: 0.25)) {
-                self.keyboardIsShowing = true
+            Task { @MainActor in
+                withAnimation(.easeInOut(duration: 0.25)) {
+                    self.keyboardIsShowing = true
+                    self.keyboardHeight = keyboardFrame?.height ?? 0
+                }
+            }
+        }
+
+        hideObserver = NotificationCenter.default.addObserver(
+            forName: UIResponder.keyboardWillHideNotification,
+            object: nil,
+            queue: .main
+        ) { [weak self] notification in
+            guard let self else { return }
+
+            Task { @MainActor in
+                withAnimation(.easeInOut(duration: 0.25)) {
+                    self.keyboardIsShowing = false
+                    self.keyboardHeight = 0
+                }
             }
         }
     }
 
-    @objc func keyboardWillHide() {
-        Task { @MainActor in
-            withAnimation(.easeInOut(duration: 0.25)) {
-                self.keyboardIsShowing = false
-            }
-        }
+    deinit {
+        if let showObserver { NotificationCenter.default.removeObserver(showObserver) }
+        if let hideObserver { NotificationCenter.default.removeObserver(hideObserver) }
     }
 }
