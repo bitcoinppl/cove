838720bbf42a4a54df0af3c5af9d299c75049233
Praveen Perera <me@praveenperera.com>
2026-05-28 14:41:02 -0500

    Bump Android versionCode to 19
    Incremented android/app versionCode from 18 to 19 to mark a new build for release. versionName remains 1.2.3.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "org.bitcoinppl.cove"
         minSdk = 33
         targetSdk = 36
-        versionCode = 18
+        versionCode = 19
         versionName = "1.2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
