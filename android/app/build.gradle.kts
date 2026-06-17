08c54551f203b8cfb620ce06b5978cfb16819b70
Praveen Perera <me@praveenperera.com>
2026-06-16 21:00:46 -0500

    Bump Android build number

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "org.bitcoinppl.cove"
         minSdk = 33
         targetSdk = 36
-        versionCode = 21
+        versionCode = 24
         versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
