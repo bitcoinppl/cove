1cc07e34e078672ebff93200e8f845fdb4017a53
Praveen Perera <me@praveenperera.com>
2026-08-27 15:56:55 -0500

    Bump iOS and android build numbers

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
         applicationId = "org.bitcoinppl.cove"
         minSdk = 33
         targetSdk = 36
-        versionCode = 38
+        versionCode = 39
         versionName = "1.4.0"
         manifestPlaceholders["keyTeleportAppLinksEnabled"] = "false"
 
